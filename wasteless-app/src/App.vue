7f4a3813291061d11c4c9427df14569856ccfaec
Luigi531 <miniloupiot@gmail.com>
2024-08-01 12:45:29 +0200

    designed navbar CSS

--- wasteless-app/src/App.vue
+++ wasteless-app/src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <nav>
-    <router-link :to="{ name: 'accueil' }">Accueil</router-link> |
-    <router-link :to="{ name: 'stocks' }">Stock</router-link> |
-    <router-link :to="{ name: 'repas' }">Repas</router-link> |
-  </nav>
+  <div class="navBar">
+    <div class="navMenu">
+      <nav class="nav">
+        <router-link :to="{ name: 'accueil' }">Accueil</router-link> 
+        <router-link :to="{ name: 'stocks' }">Stock</router-link> 
+        <router-link :to="{ name: 'repas' }">Repas</router-link> 
+      </nav>
+    </div>
+    <div class="navTitle">
+      <p>Wasteless!</p>
+    </div>
+  </div> 
   <router-view/>
 </template>
 
@@ -16,16 +23,46 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.navBar {
+  width: 100%;
+  position: relative;
+  z-index: 1000;
 }
 
-nav a {
+.navMenu {
+  padding: 9.64px 0px;
+  background-color: #000000;
+  text-align: left;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.nav a {
+  text-decoration: none;
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
+  margin: 0px;
+  border-right: solid 1px #dfb81b;
+  padding: 10px 15px;
+  z-index: 1001;
 }
 
-nav a.router-link-exact-active {
+.nav a:hover {
+  background-color: #dfb81b;
+}
+
+.nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.navTitle p {
+  color: #ffffff;
+  z-index: 1001;
+  position: relative;
+  text-align: center;
+  font-weight: bold;
+  margin-top: 9.6px; 
 }
 </style>
