db521f1accc9496e992971e3e0a37bcfba523dbc
Luigi531 <miniloupiot@gmail.com>
2024-08-02 12:23:06 +0200

    accueil ?

---
+++ wasteless-app/src/views/Acceuil.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="calendar">
+
+  </div>
+  <div class="expiration">
+
+  </div>
+  <div class="dishOffer">
+
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.calendar {
+  position: fixed;
+  top: 6.041426vh;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 20vh;
+  background-color: #656565
+}
+.expiration {
+  position: fixed;
+  top: 26.041426vh;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  width: 25vw;
+  height: 73.958574vh;
+  background-color: #e88711
+}
+.dishOffer {
+  position: fixed;
+  top: 26.041426vh;
+  left: 25vw;
+  margin: 0;
+  padding: 0;
+  width: 75vw;
+  height: 73.958574vh;
+  background-color: #ff008c
+}
+
+</style>
